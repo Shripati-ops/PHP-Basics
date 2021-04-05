29580db2cc5faa2e2c3e32a6a503360a9b6107a8
Shripati-ops <shripatia302@gmail.com>
2021-04-05 13:48:09 +0530

    "Added a function"

--- index.php
+++ index.php
@@ -6,26 +6,29 @@
 <body>
 
 	<?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $name = $_REQUEST['name'];
-	$email = $_REQUEST['email'];
-      $person = array($name,$email);
-	  var_dump($person);
-	  if($person[0] == "Shripati"||"shripati" && $person[1] == "shripatia302@gmail.com"){
-      	  echo "$person[0]" . " Has " . " Email $person[1]";
+	function accept_data(){
+     if($_SERVER["REQUEST_METHOD"] == "POST"){
+     $name = $_REQUEST['name'];
+ 	 $email = $_REQUEST['email'];
+     $person = array($name,$email);
+	 var_dump($person);
+	 if($person[0] == "Shripati"||"shripati" && $person[1] == "shripatia302@gmail.com"){
+      	 echo "$person[0]" . " Has " . " Email $person[1]";
       	}
-      }
+     }
+  }
+    accept_data();
 
-	  $a = 5;
-	  $b = 10;
-	  $txt1 = "Hello from Shripati";
-	  $len = strlen($txt1);
-	  $word_len = str_word_count($txt1);
-	  $rev = strrev($txt1);
-	  $pos = strpos($txt1, "Shripati");
-	  $replaced = str_replace("Shripati","Code", $txt1);
-	  echo "<h1> $replaced </h1>" , "<h1> $len </h1>", "<h1> $word_len </h1>","<h1> $rev </h1>","<h1> $pos </h1>";
-	  echo $a + $b; 
+	  // $a = 5;
+	  // $b = 10;
+	  // $txt1 = "Hello from Shripati";
+	  // $len = strlen($txt1);
+	  // $word_len = str_word_count($txt1);
+	  // $rev = strrev($txt1);
+	  // $pos = strpos($txt1, "Shripati");
+	  // $replaced = str_replace("Shripati","Code", $txt1);
+	  // echo "<h1> $replaced </h1>" , "<h1> $len </h1>", "<h1> $word_len </h1>","<h1> $rev </h1>","<h1> $pos </h1>";
+	  // echo $a + $b; 
       
 	?>
 
